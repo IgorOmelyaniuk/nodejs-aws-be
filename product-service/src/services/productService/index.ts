21affdedff581e2fc641f0fd91aa36c8ed59323e
Ihar Amelyaniuk <ihar_amelyaniuk@epam.com>
2020-11-01 19:53:14 +0300

    Add products

--- product-service/src/services/productService/index.ts
+++ product-service/src/services/productService/index.ts
@@ -1,9 +1,10 @@
 import products from '../../mocks/products.json';
+import { Product } from '../../types'
 
-export async function getAll(): Promise<any[]> {
+export async function getAll(): Promise<Product[]> {
   return products;
 }
 
-export async function getById(id: string): Promise<any> {
+export async function getById(id: string): Promise<Product> {
   return products.find(product => product.id === id);
 }
