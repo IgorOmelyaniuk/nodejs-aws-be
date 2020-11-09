f1b90af3d96941276347bb06358a615a2f53ed74
Ihar Amelyaniuk <ihar_amelyaniuk@epam.com>
2020-11-09 13:25:34 +0300

    Update error message

--- product-service/src/utils/getError.ts
+++ product-service/src/utils/getError.ts
@@ -4,7 +4,7 @@ export const ERROR_MESSAGES = {
   DB_GENERAL_ERROR: 'Database error',
   PRODUCTS_NOT_FOUND: 'Products are not found',
   PRODUCT_NOT_FOUND: 'Not found product with id',
-  PRODUCT_NOT_VALID: 'Error for product creation. Id, title, description, price and count should be provided in request',
+  PRODUCT_NOT_VALID: 'Error for product creation. Product data is not valid',
   PRODUCT_ID_NOT_VALID: 'Error for getting product. Id is not valid',
   PRODUCT_NOT_CREATED: 'Product is not created',
   INTERNAL_SERVER_ERROR: 'Internal server error',
