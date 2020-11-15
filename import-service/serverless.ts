4da1882fbd089b341a39159adfb17a417f322ff2
Ihar Amelyaniuk <ihar_amelyaniuk@epam.com>
2020-11-15 12:42:20 +0300

    Task 5

---
+++ import-service/serverless.ts
@@ -0,0 +1,80 @@
+import type { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'import-service',
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  plugins: ['serverless-webpack', 'serverless-dotenv-plugin'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    stage: 'dev',
+    region: 'eu-west-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      BUCKET_REGION: process.env.BUCKET_REGION,
+      BUCKET_NAME: process.env.BUCKET_NAME,
+    },
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: 's3:ListBucket',
+        Resource: "arn:aws:s3:::amelyaniuk-store-app-storage"
+      },
+      {
+        Effect: 'Allow',
+        Action: 's3:*',
+        Resource: "arn:aws:s3:::amelyaniuk-store-app-storage/*",
+      }
+    ]
+  },
+  functions: {
+    importProductsFile: {
+      handler: 'src/handlers/importProductsFile/index.handler',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'import',
+            cors: true,
+            request: {
+              parameters: {
+                querystrings: {
+                  name: true
+                }
+              }
+            }
+          }
+        }
+      ]
+    },
+    importFileParser: {
+      handler: 'src/handlers/importFileParser/index.handler',
+      events: [
+        {
+          s3: {
+            bucket: 'amelyaniuk-store-app-storage',
+            event: 's3:ObjectCreated:*',
+            rules: [{
+              prefix: 'uploaded/',
+              suffix: '',
+            }],
+            existing: true,
+          }
+        }
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration;
